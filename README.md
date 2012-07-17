af846d31d077d399c1bcfaa7aabc07febf53626d
yoav <yoav@monfort.co.il>
2012-07-17 18:41:10 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#memcached server populator from GarantiaData exported CSV format
+This utility populates a memcached server from the keys, values and metadata available in GarantiaData exported CSV files.
+
+#CSV file format
+Each row in the CSV represents a single key-value pare. The CSV has 4 columns in the following order:
+
+1. Key - C style encoded string
+2. Value - C style encoded string
+3. Flags, the memcached flags value for this key - a hexadecimal integer.
+4. Expiry time, either 0 for no expiry or the unixtime expiry value - an unsigned decimal integer.
+
+Example of a line in the CSV:
+```
+key_name,some text including some binary data:\xff\x00,1F,292161600
+```
+Note that if the key or value include any commas, forward slashes or non printable chars they'll be escaped.
---
+++ mcpopulator.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import argparse
+import socket
+import sys
+    
+# We can't use regular python-memcache since it doesn't enable explicit flag setting. Implememnt my own.
+class MemcacheRaw:
+    def __init__(self, addr='localhost', port=11211, timeout=None):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(timeout)
+        self.sock.connect((addr, port))
+        
+    def set(self, key, val, flags=0, expiry=0):
+        self.sock.sendall('set %s %d %d %d\r\n%s\r\n'%(key, flags, expiry, len(val), val))
+        if not self.sockExpect('STORED\r\n'):
+            raise Exception('Failed to store key %s'%key)
+            
+    def sockExpect(self, expect):
+        buf = ''
+        while True:
+            d = self.sock.recv(len(expect) - len(buf))
+            if not d:
+                raise Exception('Socket closed')
+            buf += d
+            if len(buf) == len(expect):
+                if buf == expect:
+                    return True
+                else:
+                    return False
+
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser(description='memcache populator.')
+    argParser.add_argument('--addr', default='localhost', help='Server address. Defaults to localhost.')
+    argParser.add_argument('--port', default=11211, type=int, help='Server port. Defaults to 11211.')
+    argParser.add_argument('csv_file', help='CSV File to read data from. See: https://github.com/GarantiaData/memcache_populator for details.')
+    
+    args = argParser.parse_args()
+    
+    # Open input file
+    try:
+        f = open(args.csv_file)
+    except Exception as e:
+        print >> sys.stderr, 'Error %s opening file: %s'%e
+        exit(1)
+
+    # Connect to server
+    try:
+        srv = MemcacheRaw(addr=args.addr, port=args.port)
+    except Exception as e:
+        print >> sys.stderr, 'Error connecting to server: %s'%e
+        exit(1)
+
+    # Parse CSV file and fill server
+    try:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            parts = line.strip().split(',')
+            srv.set(parts[0].decode('string_escape'), parts[1].decode('string_escape'), int(parts[2], 16), int(parts[3]))
+    except Exception as e:
+        print >> sys.stderr, 'Failed populating server from csv file: %s'%e
+        exit(1)
